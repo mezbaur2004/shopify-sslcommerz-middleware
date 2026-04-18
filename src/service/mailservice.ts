1210e59c439cf1124e11b8c321152cac981b3ae3
mezbaur2004 <mezbaur2004@gmail.com>
2026-04-18 15:06:53 +0600

    smtp completed

--- src/service/mailservice.ts
+++ src/service/mailservice.ts
@@ -6,13 +6,12 @@ export const sendEmail = async (
     subject: string,
     html: string
 ) => {
-    console.log("[EMAIL] sending via Brevo API...");
 
     const res = await axios.post(
         "https://api.brevo.com/v3/smtp/email",
         {
             sender: {
-                name: "Jolly Learning",
+                name: "Jolly Phonics Bangladesh",
                 email: envVars.BREVO_VERIFIED_EMAIL, // MUST be verified in Brevo
             },
             to: [{ email: to }],
@@ -27,6 +26,4 @@ export const sendEmail = async (
             timeout: 10000,
         }
     );
-
-    console.log("[EMAIL] sent:", res.data);
 };
